357138853377edb961e719db8cf7556fdaf2fe77
Joel Dietz <jdietz+github@gmail.com>
2023-03-30 19:03:46 +0300

    monitor update

--- monitor.js
+++ monitor.js
@@ -4,6 +4,8 @@ async function monitor() {
   // Launch a new browser instance
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  
+  console.log("loading...");
 
   // Set up event listeners for console logs and errors
   page.on('console', (msg) => console.log('Page log:', msg.text()));
@@ -12,8 +14,13 @@ async function monitor() {
   // Navigate to your web app
   await page.goto('http://localhost:3000');
 
+  await page.evaluate(() => {
+    window.playJourney();
+  });
+
   // Interact with the cubes and perform checks
   // Example: click a button to start the journey, check if audio is playing, etc.
+  await new Promise((resolve) => setTimeout(resolve, 1000 * 60 * 2)); // Wait for 2 minutes
 
   // Close the browser instance
   await browser.close();

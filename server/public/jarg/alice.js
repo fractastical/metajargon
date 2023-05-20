5a0c1ae8a560a77d0ab822d936ceba2819e62ff5
Joel Dietz <jdietz+github@gmail.com>
2023-05-20 15:50:05 +0300

    first refactor

---
+++ server/public/jarg/alice.js
@@ -0,0 +1,8 @@
+
+
+function yes() {
+
+    shrinkTerminal();
+    terminal.write("\r\nin order to escape the maze you must create a game in which to trap the minotaur. \r\nwhat game would you like to create?");
+  }
+  

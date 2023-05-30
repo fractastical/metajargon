ddf835f87323f8584a7a6cb70ac62486f1cb10f6
Joel Dietz <jdietz+github@gmail.com>
2023-05-30 13:09:26 +0400

    add sprint a

--- server/public/jarg/alice.js
+++ server/public/jarg/alice.js
@@ -1,8 +1,16 @@
 
 
 function yes() {
+  terminal.write("\r\nwhere are you?\r\n");
 
     shrinkTerminal();
-    terminal.write("\r\nin order to escape the maze you must create a game in which to trap the minotaur. \r\nwhat game would you like to create?");
   }
+
+  
+  function no() {
+
+    // shrinkTerminal();
+    // terminal.write("\r\nin order to escape the maze you must create a game in which to trap the minotaur. \r\nwhat game would you like to create?");
+  }
+
   

751dfcc4ae1597aab245ddac7513f38d81669338
Joel Dietz <jdietz+github@gmail.com>
2023-06-27 21:33:14 +0200

    apparent bug with routing

--- server/server.js
+++ server/server.js
@@ -94,6 +94,7 @@ const bodyParser = require('body-parser');
 // // Call the async function to connect to MongoDB
 // connectToMongoDB();
 
+//TODO: this appears to be borken now
 app.get('/', (req, res) => {
   res.send(req.user);
  });
